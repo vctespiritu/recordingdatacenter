38c05301860c056dd42144c859a977e91f58edb4
Victor Espiritu <vicespi_1123@hotmail.es>
2020-04-14 23:49:55 -0400

    Create variables needed, complete table, add button submit functionality, prepend name in table from first and last name variables

--- javascript/jquery_functions.js
+++ javascript/jquery_functions.js
@@ -1,10 +1,29 @@
 $(function(){
-    
-    var table = "<table></table>";
+
+    var first_name = "";
+    var last_name = "";
+    var data;
+    var date;
+    var ind = 0;
 
     $("#btnSubmit").click(function(){
         
+        first_name = $("#fname").val();
+        last_name = $("#lname").val();
+        date = $("#todaysdate").val();
+        data = $("#enterdata").val();
+        
+        if(ind == 0){
+
+            $(".maintable").prepend("<th colspan='2'>" + first_name + " " + last_name +  "</th>");
+            
+            ind++;
+        }
+
+        $(".maintable").append("<tr><td>" + date + "</td><td>" + data + " lb</td></tr>");
+
     });
 
+    
     
 });
